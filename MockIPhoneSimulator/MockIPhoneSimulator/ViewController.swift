6233f437b96891774527a82239f9ca741827c0d7
TopGear <tglover.nuppy@gmail.com>
2016-08-13 16:53:16 +0900

    [add] added MockIPhoneSimulator

---
+++ MockIPhoneSimulator/MockIPhoneSimulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MockIPhoneSimulator
+//
+//  Created by MasayukiHayashi on 2016/08/13.
+//  Copyright © 2016年 itolab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
