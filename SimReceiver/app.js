137c2e3b63d8d192cd92505eb7786012df5578c2
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2016-10-21 17:31:15 +0900

    wip

--- SimReceiver/app.js
+++ SimReceiver/app.js
@@ -149,7 +149,7 @@ Client.prototype.onReceiveData = function(data) {
     var mobility = data['Mobility'];
 
     if ('Name' in mobility) {
-      var name = mobility['Name'] + '(' + ip + ')';
+      var name = mobility['Name'] + ' (' + ip + ')';
       if (name && this.name !== name) {
         this.name = name;
       }
