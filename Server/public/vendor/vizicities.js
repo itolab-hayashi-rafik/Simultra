7706475d7cbe3b64e293c43f43ee1c921a152df8
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2016-10-23 17:09:53 +0900

    wip

--- Server/public/vendor/vizicities.js
+++ Server/public/vendor/vizicities.js
@@ -19286,6 +19286,26 @@ return /******/ (function(modules) { // webpackBootstrap
 	        simObject.setLabelText(text);
 	      }
 	    }
+	
+	    /**
+	     * Sets the label offset
+	     *
+	     * @param {Number} id id
+	     * @param {Number} x x
+	     * @param {Number} y y
+	     * @param {Number} z z
+	     */
+	  }, {
+	    key: 'setLabelOffset',
+	    value: function setLabelOffset(id, x, y, z) {
+	      // if the object exists
+	      if (id in this._simObjects) {
+	        var simObject = this._simObjects[id];
+	
+	        // update the offset
+	        simObject.setLabelOffset(x, y, z);
+	      }
+	    }
 	  }, {
 	    key: '_debug',
 	    value: function _debug() {
@@ -19731,13 +19751,15 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 80 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	Object.defineProperty(exports, '__esModule', {
 	  value: true
 	});
 	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 	
@@ -19747,6 +19769,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	/**
 	 * Created by masayuki on 20/07/2016.
 	 */
+	
+	var _three = __webpack_require__(10);
+	
+	var _three2 = _interopRequireDefault(_three);
 	
 	var SimObject = (function () {
 	  function SimObject() {
@@ -19763,6 +19789,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	    // 2D Object
 	    this.label = undefined;
+	    this.labelOffset = new _three2['default'].Vector3();
 	
 	    // --- construct
 	    this._createSimObject();
@@ -19800,7 +19827,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    key: 'setPosition',
 	    value: function setPosition(x, y, z) {
 	      this.root.position.set(x, y, z);
-	      this.label.position.copy(this.root.position);
+	      this.label.position.copy(this.root.position).add(this.labelOffset);
 	    }
 	
 	    /**
@@ -19845,6 +19872,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 	
 	    /**
+	     * sets the label offset
+	     *
+	     * @param {Number} x x
+	     * @param {Number} y y
+	     * @param {Number} z z
+	     */
+	  }, {
+	    key: 'setLabelOffset',
+	    value: function setLabelOffset(x, y, z) {
+	      this.labelOffset.set(x, y, z);
+	    }
+	
+	    /**
 	     * update the object
 	     * @param {Number} delta
 	     */
@@ -19858,7 +19898,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	      if (this.updatePosition) {
 	        this.root.position.x += Math.cos(-this.angle) * forwardDelta;
 	        this.root.position.z += Math.sin(-this.angle) * forwardDelta;
-	        this.label.position.copy(this.root.position);
+	        this.label.position.copy(this.root.position).add(this.labelOffset);
 	
 	        this.root.rotation.y = this.angle;
 	      }
@@ -19871,19 +19911,25 @@ return /******/ (function(modules) { // webpackBootstrap
 	      // construct this object
 	
 	      // root
-	      this.root = new THREE.Object3D();
+	      this.root = new _three2['default'].Object3D();
 	
 	      // [DEBUG] arrow
-	      var from = new THREE.Vector3(0, 0, 0);
-	      var to = new THREE.Vector3(50, 0, 0);
+	      var from = new _three2['default'].Vector3(0, 0, 0);
+	      var to = new _three2['default'].Vector3(50, 0, 0);
 	      var direction = to.clone().sub(from);
 	      var length = direction.length();
-	      var arrowHelper = new THREE.ArrowHelper(direction.normalize(), from, length, 0xff0000);
+	      var arrowHelper = new _three2['default'].ArrowHelper(direction.normalize(), from, length, 0xff0000);
 	      this.root.add(arrowHelper);
 	
 	      // label
 	      var text = document.createElement('div');
-	      this.label = new THREE.CSS2DObject(text);
+	      this.label = new _three2['default'].CSS2DObject(text);
+	    }
+	  }, {
+	    key: '_adjustLabelOffset',
+	    value: function _adjustLabelOffset() {
+	      var bbox = new _three2['default'].Box3().setFromObject(this.root);
+	      this.setLabelOffset(0, bbox.max.y + 1, 0);
 	    }
 	
 	    // ---
@@ -21575,6 +21621,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	        // finish
 	        this.add(root);
 	
+	        // adjust the label
+	        this._adjustLabelOffset();
+	
 	        // callback
 	        if (this.callback) {
 	          this.callback(self);
@@ -22224,6 +22273,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	        // finish
 	        this.add(root);
+	
+	        // adjust the label
+	        this._adjustLabelOffset();
 	
 	        // callback
 	        if (this.callback) {
