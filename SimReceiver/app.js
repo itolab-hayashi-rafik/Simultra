32f6db171e62801610734ebf2f46dfc6bb21e312
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2016-10-23 15:59:39 +0900

    wip

--- SimReceiver/app.js
+++ SimReceiver/app.js
@@ -205,7 +205,7 @@ Client.prototype.updateVehicle = function(mobility) {
     changed = true;
   }
   if ('Speed' in mobility) {
-    vehicle.velocity = mobility['speed'];
+    // vehicle.velocity = mobility['speed'];
     changed = true;
   }
 
